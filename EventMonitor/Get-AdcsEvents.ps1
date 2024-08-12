0dbb6c78b6f0eae003b3a7c14ae0fab46326e6a5
Uwe Gradenegger <uwe.gradenegger@outlook.de>
2024-08-12 14:57:04 +0200

    Update scripts

---
+++ EventMonitor/Get-AdcsEvents.ps1
@@ -0,0 +1,149 @@
+﻿param (
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $From = "$($env:ComputerName)@domain.tld",
+    
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [String[]]
+    $To = ("admin1@domain.tld", "admin2@domain.tld"),
+    
+    [Parameter(Mandatory=$False)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $SmartHost = "my.smart.host",
+
+    [Parameter(Mandatory=$False)]
+    [ValidateRange(1,10)]
+    [Int]
+    $Retries = 5,
+    
+    [Parameter(Mandatory=$False)]
+    [ValidateRange(1,1440)]
+    [Int]
+    $Minutes = 10
+)
+
+begin {
+
+$Head = @'
+<style type="text/css">
+<!-
+
+table {
+  border-collapse: collapse;
+  border: 1px solid black;
+}
+
+table th, td {
+  border: 1px solid black;
+}
+->
+</style>
+'@
+
+}
+
+process {
+
+    $StartTime = (Get-Date).AddMinutes(-$Minutes)
+    $HostDisplayName = (Get-WmiObject Win32_ComputerSystem).DNSHostName
+
+    $Events = @()
+
+    If (Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Services\CertSvc\Configuration") {
+
+        $CaName = $(Get-ItemPropertyValue -Path "HKLM:\SYSTEM\CurrentControlSet\Services\CertSvc\Configuration" -Name "Active")
+        $HostDisplayName = "$HostDisplayName ($CaName)"
+
+        $Events += Get-WinEvent -FilterHashtable @{
+            Logname='Application'
+            ProviderName=@('Microsoft-Windows-CertificationAuthority')
+            Id=@("26","15","38","46","55","60","65","67","74","95","130")   
+            StartTime=($StartTime)
+        }, @{
+            Logname='Application'
+            ProviderName=@('TameMyCerts')
+            Id=@("6","8","10")
+            StartTime=($StartTime)
+        } -ErrorAction SilentlyContinue | Sort-Object -Property TimeCreated -Descending
+    }
+
+    If (Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Services\OcspSvc\Responder") {
+
+        $Events += Get-WinEvent -FilterHashtable @{
+            Logname='Application'
+            ProviderName=@('Microsoft-Windows-OnlineResponder')
+            Id=@("20","21","22","23","25","26","27","29","33","34","35")
+            StartTime=($StartTime)
+        }, @{
+            Logname='Application'
+            ProviderName=@('Microsoft-Windows-OnlineResponderRevocationProvider')
+            Id=@("16","17","18")
+            StartTime=($StartTime)
+        },@{
+            Logname='Application'
+            ProviderName=@('Microsoft-Windows-OnlineResponderWebProxy')
+            Id=@("17","21")
+            StartTime=($StartTime)
+        } -ErrorAction SilentlyContinue | Sort-Object -Property TimeCreated -Descending
+    }
+
+    If (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Cryptography\MSCEP") {
+
+        $Events += Get-WinEvent -FilterHashtable @{
+            Logname='Application'
+            ProviderName=@('Microsoft-Windows-NetworkDeviceEnrollmentService')
+            Id=@("2","4","8","9","10","34","35","44","49","51")
+            StartTime=($StartTime)
+        } -ErrorAction SilentlyContinue | Sort-Object -Property TimeCreated -Descending
+    }
+
+    If ($Events) {
+
+        # Send Notifications for each distinct Event Id
+        $Events.Id | Sort-Object | Get-Unique | ForEach-Object -Process {
+
+            $EventId = $_
+            $PartialEvents = $Events | Where-Object { $_.Id -eq $EventId }
+            $EventSource = ($PartialEvents | Select-Object -First 1).ProviderName
+
+            $Subject = "PKI Event Monitor $HostDisplayName EventID $EventId from Source $EventSource"
+         
+			# Build HTML Table out of the identified Events
+            $Body = $PartialEvents | Select-Object -Property TimeCreated,Message | ConvertTo-HTML -Fragment
+			
+			# Convert Array to one String
+            $Body = (ConvertTo-HTML -Body $Body -Head $Head) -join ""
+
+            # Logic to retry on failure
+            For ($i = 1; $i -le $Retries; $i++) {
+
+                Try {
+            
+                    Send-MailMessage `
+                        -SmtpServer $SmartHost `
+                        -From $From `
+                        -To $To `
+                        -Subject $Subject `
+                        -Body $Body `
+                        -BodyAsHtml `
+                        -Priority High `
+                        -ErrorAction Stop
+
+                    # Quit the loop when we're done
+                    return
+                }
+                Catch {
+                    Write-Error -Message $_.Exception.Message
+                }
+
+                # Only if it did not work the first time
+                Start-Sleep -Seconds 10 
+            }
+        }
+    }
+}
+
+end {}
--- NDESAutoRenew/Get-NDESRACertificates.ps1
+++ NDESAutoRenew/Get-NDESRACertificates.ps1
@@ -1,5 +1,6 @@
 #Requires -Modules @{ ModuleName="PSCertificateEnrollment"; ModuleVersion="1.0.6" },@{ ModuleName="WebAdministration"; ModuleVersion="1.0.0.0" }
 
+[cmdletbinding()]
 param(
     [Parameter(Mandatory=$True)]
     [ValidateNotNullOrEmpty()]
@@ -31,6 +32,16 @@ begin {
     New-Variable -Option Constant -Name szOID_CERTIFICATE_TEMPLATE -Value "1.3.6.1.4.1.311.21.7"
     New-Variable -Option Constant -Name XCN_CRYPT_STRING_BASE64 -Value 1
 
+    New-Variable -Option Constant -Name DispositionType -Value @{
+        CR_DISP_INCOMPLETE = 0
+        CR_DISP_ERROR = 1
+        CR_DISP_DENIED = 2
+        CR_DISP_ISSUED = 3
+        CR_DISP_ISSUED_OUT_OF_BAND = 4
+        CR_DISP_UNDER_SUBMISSION = 5
+        CR_DISP_REVOKED = 6
+    }
+
     function Get-TemplateOidFromRegistry {
 
         param(
@@ -40,7 +51,7 @@ begin {
             $Name
         )
 
-        return (Get-Itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Cryptography\CertificateTemplateCache\$Name")."msPKI-Cert-Template-OID"
+        return (Get-Itemproperty -Path "HKLM:\SOFTWARE\Microsoft\Cryptography\CertificateTemplateCache\$Name" -ErrorAction SilentlyContinue)."msPKI-Cert-Template-OID"
     }
 
     function Get-NdesRaCertificate {
@@ -78,10 +89,7 @@ begin {
     function Set-PrivateKeyPermissions {
 
         param(
-            [Parameter(
-                Mandatory=$True,
-                ValuefromPipeline=$True
-                )]
+            [Parameter(Mandatory=$True, ValuefromPipeline=$True)]
             [System.Security.Cryptography.X509Certificates.X509Certificate]
             $Certificate,
 
@@ -108,53 +116,70 @@ begin {
 process {
 
     # Prevent usage on PowerShell Core
-    if ($PSVersionTable.PSEdition -ne "Desktop") {
+    If ($PSVersionTable.PSEdition -ne "Desktop") {
         Write-Error -Message "This is only compatible with the Desktop Edition of Windows PowerShell"
         return
     }
 
     # Ensuring the Script will be run with Elevation
-    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    If (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
         Write-Error -Message "This must be run as Administrator! Aborting."
-        Return
+        return
     }
 
     # Abort if NDES is not installed
-    $ConfigString = (Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Cryptography\MSCEP\CAInfo -Name Configuration -ErrorAction).Configuration
-    if ($null -eq $ConfigString) {
+    $ConfigString = (Get-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Cryptography\MSCEP\CAInfo -Name Configuration -ErrorAction SilentlyContinue).Configuration
+    If ($null -eq $ConfigString) {
         Write-Error -Message "This host does not seem to have NDES installed. Aborting."
+        return
     }
 
     $RestartRequired = $False
 
-    [PSCustomObject]@{
-        Name = $EnrollmentAgentTemplate
-        KeyUsage = "DigitalSignature"
-        Csp = $Csp
-    },
-    [PSCustomObject]@{
-        Name = $CepEncryptionTemplate
-        KeyUsage = "KeyEncipherment"
-        Csp = $Csp
-    } | 
-    ForEach-Object -Process {
-
-        $CurrentTemplate = $_
-
-        if (-not (Get-NdesRaCertificate -Oid (Get-TemplateOidFromRegistry -Name $CurrentTemplate.Name) -Days $Days)) {
-
-            Write-Verbose -Message "Requesting certificate for $($CurrentTemplate.Name) template from $ConfigString"
-
-            $csr = New-CertificateRequest -Subject "CN=$env:ComputerName" -MachineContext -Ksp $CurrentTemplate.Csp -KeyLength 4096 -KeyUsage $CurrentTemplate.KeyUsage
-            $cert = $csr | Get-IssuedCertificate -ConfigString $ConfigString -CertificateTemplate $CurrentTemplate.Name
-            $cert.Certificate | Install-IssuedCertificate -MachineContext -Force | Out-Null
-            $cert.Certificate | Set-PrivateKeyPermissions
-
-            $RestartRequired = $True
+    $TemplateCollection =
+        [PSCustomObject]@{
+            Name = $EnrollmentAgentTemplate
+            KeyUsage = "DigitalSignature"
+            Csp = $Csp
+        },
+        [PSCustomObject]@{
+            Name = $CepEncryptionTemplate
+            KeyUsage = "KeyEncipherment"
+            Csp = $Csp
         }
+
+    ForEach ($CurrentTemplate in $TemplateCollection) {
+
+        If (Get-NdesRaCertificate -Oid (Get-TemplateOidFromRegistry -Name $CurrentTemplate.Name) -Days $Days) {
+            Write-Verbose -Message "Found a valid certificate for $($CurrentTemplate.Name)"
+            continue
+        }
+
+        Write-Verbose -Message "Requesting certificate for $($CurrentTemplate.Name) template from $ConfigString"
+
+        $CertificateRequest = New-CertificateRequest `
+            -Subject "CN=$env:ComputerName" `
+            -MachineContext `
+            -Ksp $CurrentTemplate.Csp `
+            -KeyLength 4096 `
+            -KeyUsage $CurrentTemplate.KeyUsage
+
+        $SubmissionResponse = $CertificateRequest | Get-IssuedCertificate `
+            -ConfigString $ConfigString `
+            -CertificateTemplate $CurrentTemplate.Name
+
+        If ($SubmissionResponse.Disposition -ne $DispositionType.CR_DISP_ISSUED) {
+            Write-Warning -Message "Unable to retrieve certificate for $($CurrentTemplate.Name) from $($ConfigString): $($SubmissionResponse.StatusMessage)"
+            continue
+        }
+
+        $SubmissionResponse.Certificate | Install-IssuedCertificate -MachineContext -Force | Out-Null
+        $SubmissionResponse.Certificate | Set-PrivateKeyPermissions
+
+        $RestartRequired = $True        
     }
 
-    if ($RestartRequired) {
+    If ($RestartRequired) {
 
         Restart-WebAppPool -Name SCEP
         Start-Sleep -Seconds 15
